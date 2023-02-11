1ad787ef0fd0909a2bf2aa961463c4e68503ab93
Zachariah Jeter <Zachjeter95@icloud.com>
2023-02-11 03:24:50 -0800

    local storage added. additonal styles added

--- app.js
+++ app.js
@@ -79,6 +79,10 @@ function handleClick(event) {
   if(votingRounds === 0){
     imgContainer.removeEventListener('click', handleClick);
   }
+  let stringifiedPictures = JSON.stringify(picArray);
+  localStorage.setItem('myPictures', stringifiedPictures);
+  console.log('Stringified data', stringifiedPictures);
+
 }
 
 function handleResults(){
